6d15c482d0003d6d6a47998dc6f85f44b4748979
Soul Shard <191157967+HvTcCore@users.noreply.github.com>
2025-06-28 00:52:47 +0700

    1

--- patch/download.sh
+++ patch/download.sh
@@ -1,10 +1,14 @@
 
-MAGISKVERSION="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.version")"
-MAGISKLINK="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.link")"
-echo "Magisk stable version is $MAGISKVERSION"
+##MAGISKVERSION="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.version")"
+##MAGISKLINK="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.link")"
+##echo "Magisk stable version is $MAGISKVERSION"
 
-wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-release.apk -O canary.apk
-wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk -O debug.apk
-wget https://raw.githubusercontent.com/HuskyDG/magisk-files/main/app-release.apk -O canary-huskydg.apk
-wget https://raw.githubusercontent.com/HuskyDG/magisk-files/main/app-debug.apk -O debug-huskydg.apk
-wget $MAGISKLINK -O stable.apk
+##wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-release.apk -O canary.apk
+##wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk -O debug.apk
+##wget https://raw.githubusercontent.com/HuskyDG/magisk-files/main/app-release.apk -O canary-huskydg.apk
+##wget https://raw.githubusercontent.com/HuskyDG/magisk-files/main/app-debug.apk -O debug-huskydg.apk
+##wget $MAGISKLINK -O stable.apk
+
+echo "Magisk stable version is 27.0 / 27.2" #Override
+wget https://github.com/topjohnwu/Magisk/releases/download/v27.0/Magisk-v27.0.apk -O stable.apk
+wget https://github.com/1q23lyc45/KitsuneMagisk/releases/download/v27.2-kitsune-4/app-release.apk -O stable-kitsune.apk
